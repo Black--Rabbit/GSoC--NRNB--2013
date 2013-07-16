520a73e3a56cab42d40d164fed04bbde5d60e83e
Abhinav Misra <abhinavmisra919@gmail.com>
2013-07-16 22:10:47 +0530

    Update CyActivator.java

--- src/main/java/org/cytoscape/sample/internal/CyActivator.java
+++ src/main/java/org/cytoscape/sample/internal/CyActivator.java
@@ -15,6 +15,18 @@ import org.cytoscape.service.util.AbstractCyActivator;
 import java.util.Properties;
 
 
+
+import java.util.*;
+import org.cytoscape.model.*;
+import javax.swing.event.TableModelEvent;
+import org.cytoscape.browser.internal.util.TableBrowserUtil;
+import org.cytoscape.model.CyIdentifiable;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyTable;
+import org.cytoscape.model.CyTableManager;
+
+
+
 public class CyActivator extends AbstractCyActivator {
 	public CyActivator() {
 		super();
@@ -24,6 +36,9 @@ public class CyActivator extends AbstractCyActivator {
 	public void start(BundleContext bc) {
 
 		CySwingApplication cytoscapeDesktopService = getService(bc,CySwingApplication.class);
+
+		CyNetwork currentNetwork = CyApplicationManager.getCurrentNetwork();
+		CyTable nodeTable = CyNetworkTableManager.getTable(currentNetwork, CyNode.class, CyNetwork.DEFAULT_ATTRS);
 		
 		MyCytoPanel myCytoPanel = new MyCytoPanel();
 		Sample02 sample02Action = new Sample02(cytoscapeDesktopService,myCytoPanel);
@@ -35,4 +50,3 @@ public class CyActivator extends AbstractCyActivator {
 
 	}
 }
-
